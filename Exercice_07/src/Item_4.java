46a1b277ed58ee3a205cd14778053f823c8099d4
Valentin M <valentinmondor@yahoo.fr>
2021-12-02 11:28:05 +0100

    refactor: add exercices Item 1 2 3 4

--- Exercice_07/src/Item_4.java
+++ Exercice_07/src/Item_4.java
@@ -1,11 +1,23 @@
+import java.math.BigDecimal;
 
 public class Item_4 {
 
 	public static void main(String[] args) {
-		for(short i = 0; i < 40000; i++) {
-			System.out.println(i);
-		}
-
+		double deux = 456789.7890123456789;
+		char chara = (char) deux;
+		int inte = (int) deux;
+		float floater = (float) deux;
+		byte bt = (byte) deux;
+		short sht = (short) deux;
+		long lg = (long) deux;
+		
+		System.out.println("double : " + deux);
+		System.out.println("char : " + chara);
+		System.out.println("int : " + inte);
+		System.out.println("float : " + floater);
+		System.out.println("byte : " + bt);
+		System.out.println("short : " + sht);
+		System.out.println("short : " + lg);
 	}
 
 }
